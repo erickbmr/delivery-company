f5482744fa8deca924eee5f0613731c8bf192d59
erickbmr <erickbrener369@gmail.com>
2021-01-10 21:59:06 -0300

    add all select and insert funcs

--- delivery/src/main/java/FakeBanco.java
+++ delivery/src/main/java/FakeBanco.java
@@ -18,7 +18,7 @@ public class FakeBanco
         funcionarios.add(new Funcionario("Motorista2", "22222222222", "555555556"));
         veiculos.add(new Veiculo("5345345534", "CBA1D55", "Preto", 80));
         veiculos.add(new Veiculo("5345345534", "ABC1D45", "Prata", 50));
-        depositos.add(new Deposito("Brasil", "Centro", "36400000", "MG", 20, 3000));
+        depositos.add(new Deposito("Brasil", "Centro", "36400000", "MG", 20, 3000, 1212));
         plataformas.add(new Plataforma("Americanas", "02391962000144", "E-commerce", "55567676"));
         destinatarios.add(new Destinatario("Erick", "80735747059", "Sete", "36400000",
             "Alto", "MG", "12345678", 10));
@@ -26,6 +26,7 @@ public class FakeBanco
         //itens.add(new Item(2, "Mi band 4", true, 3, 189.9));
     }
     
+    //select from where
     public static Plataforma recuperaPlataforma(String documento)
     {
         for(Plataforma p : plataformas)
@@ -48,6 +49,74 @@ public class FakeBanco
         return null;
     }
     
+    public static Funcionario recuperaFuncionario(String documento)
+    {
+        for(Funcionario f: funcionarios)
+        {
+            if(f.getDocumento().equals(documento))
+                return f;
+        }
+        
+        return null;
+    }
+    
+    public static Veiculo recuperaVeiculo(String placa)
+    {
+        for(Veiculo v: veiculos)
+        {
+            if(v.getPlaca().equals(placa))
+                return v;
+        }
+        
+        return null;
+    }
+    
+    public static Item recuperaItem(int codigo)
+    {
+        for(Item i: itens)
+        {
+            if(i.getCodigo() == codigo)
+                return i;
+        }
+        
+        return null;
+    }
+    
+    public static Deposito recuperaDesposito(int codigo)
+    {
+        for(Deposito d: depositos)
+        {
+            if(d.getCodigo() == codigo)
+                return d;
+        }
+        
+        return null;
+    }
+    
+    public static RequisicaoServico recuperaRequisicaoServico(int codigo)
+    {
+        for(RequisicaoServico r: servicos)
+        {
+            if(r.getCodigo() == codigo)
+                return r;
+        }
+        
+        return null;
+    }
+    
+    //select*
+    public static void listarDepositos()
+    {
+        for(Deposito d: depositos)
+        {
+            System.out.println("Cod. Deposito: " + d.getCodigo());
+            System.out.println("Rua " + d.getRua() + "n." + d.getNumero() 
+                    + "\nBairro " + d.getBairro() + " | CEP " + d.getCep());
+            System.out.println(d.getEstado() + "\n");
+        }
+    }
+    
+    //insert
     public static void insereItem(Item novoItem)
     {
         itens.add(novoItem);
@@ -68,6 +137,19 @@ public class FakeBanco
         plataformas.add(novaPlataforma);
     }
     
+    public static void insereDeposito(Deposito novoDeposito)
+    {
+        depositos.add(novoDeposito);
+    }
     
+    public static void insereFuncionario(Funcionario novoFuncionario)
+    {
+        funcionarios.add(novoFuncionario);
+    }
+    
+    public static void insereVeiculo(Veiculo novoVeiculo)
+    {
+        veiculos.add(novoVeiculo);
+    }
     
 }
